714de575ee780a16b966c877df2f6188cd95db48
Kristjan Oopkaup <kristjan.oopkaup@gmail.com>
2018-09-05 17:06:31 +0300

    Added option to combine bacteria and archaea

--- python/kaiju_to_table.py
+++ python/kaiju_to_table.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 '''
-Merge kaiju report files for a given taxonomic rank to single table. Can be used to separate bacteria and archaea.
+Merge kaiju summary report files for a given taxonomic rank to single table. Can be used to separate bacteria and archaea.
 Proportions are calculated separately for bacteria and archaea.
+kaijuReport must be run first with argument -p (Print full taxon path); otherwise it is not possible to assign taxons
 Run as:
 kaiju_to_table.py input_files
 '''
 __author__ = "Kristjan Oopkaup"
 __license__ = "GPL"
-__version__ = "0.5"
+__version__ = "0.7"
 __maintainer__ = "Kristjan Oopkaup"
 __email__ = "kristjan.oopkaup@gmail.com"
 __status__ = "Production"
@@ -30,23 +31,42 @@ def taxaFunc(line, kingdom, total, i):
         else:
             taxaDict[taxon].append(absolute)
         total += absolute
+    elif kingdom == 'Both':
+        taxon = line.split('\t')[2].split(';')[-2].strip()
+        absolute = int(line.split('\t')[1])
+        if taxon not in taxaDict:
+            for zeros in range(1, i):
+                taxaDict[taxon].append(0)
+            taxaDict[taxon].append(absolute)
+        else:
+            taxaDict[taxon].append(absolute)
+        total += absolute
     else:
         pass
     return taxaDict, total
 
 ####  Program  ####
 
-kingdom = int(input('Do you want bacteria [1] or archaea [2]?\n'))
+kingdom = int(input('Do you want bacteria [1], archaea [2] or both [3]?\n'))
 taxonLvl = int(input('What taxonomic rank you have in your input files?\n'
     'Phylum [1],Class [2],Order [3],Family [4],Genus [5],Species [6]\n'))
-numbers = int(input('Do you want to represent proportions [1] or absolute numbers [2]?\n'))
+numbers = int(input('Do you want to represent proportions [1] or sequence counts [2]?\n'))
 
 taxa = ['kingdom','phylum','class','order','family','genus','species']
 
-if kingdom == 1:
-    table_out = open('kaiju_table_bacteria_'+taxa[taxonLvl]+'.csv', 'w+')
-elif kingdom == 2:
-    table_out = open('kaiju_table_archaea_'+taxa[taxonLvl]+'.csv', 'w+')
+if kingdom == 1 and numbers == 1:
+    table_out = open('kaiju_table_bacteria_proportions_'+taxa[taxonLvl]+'.csv', 'w+')
+elif kingdom == 1 and numbers == 2:
+    table_out = open('kaiju_table_bacteria_count_'+taxa[taxonLvl]+'.csv', 'w+')
+elif kingdom == 2 and numbers == 1:
+    table_out = open('kaiju_table_archaea_proportions_'+taxa[taxonLvl]+'.csv', 'w+')
+elif kingdom == 2 and numbers == 2:
+    table_out = open('kaiju_table_archaea_count_'+taxa[taxonLvl]+'.csv', 'w+')
+elif kingdom == 3 and numbers == 1:
+    table_out = open('kaiju_table_proportions_'+taxa[taxonLvl]+'.csv', 'w+')
+elif kingdom == 3 and numbers == 2:
+    table_out = open('kaiju_table_count_'+taxa[taxonLvl]+'.csv', 'w+')
+
 
 taxaDict = defaultdict(list)
 firstLine = taxa[taxonLvl]
@@ -61,6 +81,8 @@ for i in range(1, len(sys.argv)):
                 taxaDict, total = taxaFunc(line, 'Bacteria', total, i)
             elif kingdom == 2:
                 taxaDict, total = taxaFunc(line, 'Archaea', total, i)
+            elif kingdom == 3:
+                taxaDict, total = taxaFunc(line, 'Both', total, i)
         for k, v in taxaDict.items():
             for zeros in range(1, i-len(v)+1):
                 taxaDict[k].append(0)
